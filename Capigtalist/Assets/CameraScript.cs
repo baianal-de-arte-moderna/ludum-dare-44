2b08b946a943556c7dcf53bba6f518c9d509b816
Aléxis Kiosia <alexis.kiosia@gmail.com>
2019-04-27 15:50:56 -0300

    adding camera, player spawn

---
+++ Capigtalist/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraScript : MonoBehaviour
+{
+    Collider2D cameraCollider;
+    CinemachineConfiner cameraConfine;
+
+    void Start()
+    {
+        cameraConfine = GetComponent<CinemachineConfiner>();
+    }
+
+    void FixedUpdate()
+    {
+        if (cameraCollider == null)
+        {
+            cameraCollider = GameObject.FindGameObjectWithTag("CameraCollider").GetComponent<Collider2D>();
+            cameraConfine.m_BoundingShape2D = cameraCollider;
+        }
+
+        
+    }
+}
---
+++ Capigtalist/Assets/Scene1Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scene1Script : MonoBehaviour
+{
+    public GameObject playerSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += SetPlayerScript;
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+
+    public void SetPlayerScript(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= SetPlayerScript;
+        var rootObject = scene.GetRootGameObjects()[0];
+        rootObject.transform.position = playerSpawn.transform.position;
+    }
+}
