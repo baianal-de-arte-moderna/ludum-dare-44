8ecd2378f0e8d706a789b4201650fc89f835d081
Piramba <avpaganini@gmail.com>
2019-04-27 09:45:05 -0300

    Inicio da lixeira

---
+++ Capigtalist/Assets/Lixeira/LixoExplodeScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LixoExplodeScript : MonoBehaviour
+{
+    SpriteRenderer render;
+    Rigidbody2D rigid;
+    Vector2 direction;
+    float force;
+    float vanishSpeed;
+    bool active;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
+
+        direction = Random.insideUnitCircle;
+        direction = new Vector2(
+            direction.x,
+            Mathf.Abs(direction.y)
+        ).normalized;
+        force = 200f;
+        active = false;
+    }
+
+    void FixedUpdate()
+    {
+        render.color = Color.Lerp (
+            render.color,
+            Color.clear,
+            vanishSpeed
+        );
+        if (render.color.a < 0.2f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Explode()
+    {
+        render.enabled = true;
+        rigid.AddForce(direction * rigid.mass * force);
+    }
+}
---
+++ Capigtalist/Assets/Lixeira/LixoScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LixoScript : MonoBehaviour
+{
+    SpriteRenderer render;
+    Rigidbody2D rigid;
+    Collider2D col;
+    LixoExplodeScript[] explosionPieces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            col.enabled = false;
+            render.enabled = false;
+            foreach(var piece in explosionPieces)
+            {
+                piece.Explode();
+            }
+        }
+    }
+
+    public void Throw(Vector2 direction, float force)
+    {
+        render.enabled = true;
+        rigid.AddForce(direction * rigid.mass * force);
+    }
+}
