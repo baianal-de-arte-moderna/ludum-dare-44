e1bbfe78e6dd07656b53aca3b2ad322be5da8ff0
Piramba <avpaganini@gmail.com>
2019-04-27 02:52:25 -0300

    Inimigo base Script com cachorro exemplo

--- Capigtalist/Assets/Cachorro/CachorroScript.cs
+++ Capigtalist/Assets/Cachorro/CachorroScript.cs
@@ -14,6 +14,8 @@ public class CachorroScript : MonoBehaviour
     InimigoBaseScript inimigoBase;
     CachorroStates state;
     Rigidbody2D rigid;
+    public Vector2 LeapDirection;
+    public Collider2D TriggerCollider;
     void Awake()
     {
         inimigoBase = GetComponent<InimigoBaseScript>();
@@ -26,7 +28,21 @@ public class CachorroScript : MonoBehaviour
         if (state == CachorroStates.RUNNING)
         {
             rigid.velocity = new Vector2(-inimigoBase.speed, rigid.velocity.y);
+        } else if (state == CachorroStates.ATTACKING)
+        {
+            TriggerCollider.enabled = false;
+            rigid.AddForce(LeapDirection.normalized * inimigoBase.speed * rigid.mass * 100f);
+            state = CachorroStates.WAITING;
+            // Temporary animation resume by timing
+            // Later on will be controlled by Animator
+            Invoke("LeapEnd", 1.2f);
         }
+    }
+
+    void LeapEnd()
+    {
+        state = CachorroStates.RUNNING;
+        TriggerCollider.enabled = true;
     }
 
     void OnBecameVisible()
@@ -37,5 +53,11 @@ public class CachorroScript : MonoBehaviour
     void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        TriggerCollider.enabled = false;
+        state = CachorroStates.ATTACKING;
     }
 }
