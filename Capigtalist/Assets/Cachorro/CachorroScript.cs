cffd45c6612ed034753ede67ece776e3ac8b53b9
Piramba <avpaganini@gmail.com>
2019-04-27 01:00:57 -0300

    Inimigo base Script com cachorro exemplo

--- Capigtalist/Assets/Cachorro/CachorroScript.cs
+++ Capigtalist/Assets/Cachorro/CachorroScript.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InimigoBaseScript))]
 public class CachorroScript : MonoBehaviour
 {
-    void Start()
+    enum CachorroStates 
     {
-        
+        WAITING,
+        RUNNING,
+        ATTACKING
+    }
+    InimigoBaseScript inimigoBase;
+    CachorroStates state;
+    Rigidbody2D rigid;
+    void Awake()
+    {
+        inimigoBase = GetComponent<InimigoBaseScript>();
+        rigid = GetComponent<Rigidbody2D>();
+        state = CachorroStates.WAITING;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (state == CachorroStates.RUNNING)
+        {
+            rigid.velocity = new Vector2(-inimigoBase.speed, rigid.velocity.y);
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        state = CachorroStates.RUNNING;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
