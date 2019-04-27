4078ab0d885e88cc3fd6c242a99dde6f4e6c046c
Aléxis Kiosia <alexis.kiosia@gmail.com>
2019-04-27 18:00:50 -0300

    adding parallax scene

---
+++ Capigtalist/Assets/Level1BG/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float lenght, startPos;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float distance = (camera.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
