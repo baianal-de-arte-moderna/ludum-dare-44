7d967a3b95404508ab5b675ce7cc80b5b241965a
Piramba <avpaganini@gmail.com>
2019-04-28 19:09:57 -0300

    Boss Room done

--- Capigtalist/Assets/Level1/BossRoomScript.cs
+++ Capigtalist/Assets/Level1/BossRoomScript.cs
@@ -7,14 +7,13 @@ public class BossRoomScript : MonoBehaviour
     public Collider2D PlayerEnterTrigger;
     public Collider2D RoomBarrierCollider;
     public RoomBarrierSpriteScript RoomBarrierSprite;
-    // Start is called before the first frame update
+    
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.tag);
         if (other.CompareTag("Player"))
         {
-            PlayerEnterTrigger.enabled = false;
             RoomBarrierCollider.enabled = true;
+            PlayerEnterTrigger.enabled = false;
             RoomBarrierSprite.enabled = true;
         }
     }
