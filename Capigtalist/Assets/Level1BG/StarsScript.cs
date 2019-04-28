d09f018befd70b313c19f2831a07dc6181e4a04f
Aléxis Kiosia <alexis.kiosia@gmail.com>
2019-04-27 21:09:53 -0300

    painting beautiful stars on the night sky

---
+++ Capigtalist/Assets/Level1BG/StarsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsScript : MonoBehaviour
+{
+    Transform playerTransform;
+    SpriteRenderer starRenderer;
+    public float levelSize;
+    float previousX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform  = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        starRenderer = GetComponent<SpriteRenderer>();
+        updatePreviousXPosition();
+    }
+
+    void FixedUpdate()
+    {
+        if(playerTransform.position.x > previousX)
+        {
+            starRenderer.color = new Color(1, 1, 1, previousX/levelSize);
+            updatePreviousXPosition();
+        }
+    }
+
+    void updatePreviousXPosition()
+    {
+        previousX = playerTransform.position.x;
+    }
+}
