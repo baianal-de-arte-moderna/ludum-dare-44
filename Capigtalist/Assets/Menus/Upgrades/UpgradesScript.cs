9c3b7120eaf220ed34f337f9ce6455ae99c9be4d
Vinicius Uzeda <vruzeda@gmail.com>
2019-04-29 00:43:46 -0300

    Improved upgrade menu

--- Capigtalist/Assets/Menus/Upgrades/UpgradesScript.cs
+++ Capigtalist/Assets/Menus/Upgrades/UpgradesScript.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 public class UpgradesScript : MonoBehaviour
 {
     [SerializeField]
+    private Button regularJumpButton;
+    [SerializeField]
     private Text regularJumpPriceText;
     [SerializeField]
+    private GameObject regularJumpPricePanel;
+    [SerializeField]
+    private GameObject regularJumpInUseText;
+
+    [SerializeField]
+    private Button springJumpButton;
+    [SerializeField]
     private Text springJumpPriceText;
+    [SerializeField]
+    private GameObject springJumpPricePanel;
+    [SerializeField]
+    private GameObject springJumpInUseText;
 
     private PlayerAttributes playerAttributes;
 
@@ -46,6 +59,16 @@ public class UpgradesScript : MonoBehaviour
     {
         regularJumpPriceText.text = $"{CalculateJumpPrice(regularJumpBehaviour)}";
         springJumpPriceText.text = $"{CalculateJumpPrice(springJumpBehaviour)}";
+
+        bool regularJumpSelected = GameData.jumpBehaviour.GetType() == regularJumpBehaviour.GetType();
+        regularJumpButton.interactable = !regularJumpSelected;
+        regularJumpPricePanel.SetActive(!regularJumpSelected);
+        regularJumpInUseText.SetActive(regularJumpSelected);
+
+        bool springJumpSelected = GameData.jumpBehaviour.GetType() == springJumpBehaviour.GetType();
+        springJumpButton.interactable = !springJumpSelected;
+        springJumpPricePanel.SetActive(!springJumpSelected);
+        springJumpInUseText.SetActive(springJumpSelected);
     }
 
     private float CalculateJumpPrice(JumpBehaviour jumpBehaviour)
